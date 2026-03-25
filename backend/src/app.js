9bca3c1d862b4e276dbaf9ce1e834eba64cad279
git-devtest <jhonmaster+git@gmail.com>
2026-03-25 00:23:14 -0500

    feat(api): mount otros_servicios router as nested resource of usuarios

--- backend/src/app.js
+++ backend/src/app.js
@@ -53,6 +53,7 @@ app.use('/api/v1/facturas/:factura_id/usuarios/:usuario_id/urgencias',        re
 app.use('/api/v1/facturas/:factura_id/usuarios/:usuario_id/hospitalizaciones',require('./routes/hospitalizaciones.routes'));
 app.use('/api/v1/facturas/:factura_id/usuarios/:usuario_id/recien_nacidos',  require('./routes/recien_nacidos.routes'));
 app.use('/api/v1/facturas/:factura_id/usuarios/:usuario_id/medicamentos',    require('./routes/medicamentos.routes'));
+app.use('/api/v1/facturas/:factura_id/usuarios/:usuario_id/otros_servicios', require('./routes/otros_servicios.routes'));
 app.use('/api/v1/rips', require('./routes/rips.routes'));
 app.use('/api/v1/ref',  require('./routes/referencias.routes'));
 
